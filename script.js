8d36f2a921f83f7a0a26d3dd790a14aef1199e60
Asta Vittrup Graversen <astavittrupgraversen@Astas-MacBook-Pro-3.local>
2019-06-16 14:58:15 +0200

    ..

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+	console.log("sidenVises");
+	document.querySelector("#menuknapMobil").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+	console.log("toggleMenu");
+	document.querySelector("#menuMobil").classList.toggle("hidden");
+
+	let erSkjult = document.querySelector("#menuMobil").classList.contains("hidden");
+
+	if (erSkjult == true) {
+		document.getElementById("menuknapMobil").src = "Icons/Menu.svg";
+	} else {
+		document.getElementById("menuknapMobil").src = "icons/No.svg";
+	}
+}
